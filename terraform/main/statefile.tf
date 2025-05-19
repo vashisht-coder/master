8ac418c3b19988c1f2947c761a808ab0fb1c777a
vashisht-coder <vashishtshubham04@gmail.com>
2025-05-19 23:10:01 +0530

    Delete terraform directory

--- terraform/main/statefile.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket  = "github-terraform-bucket"
-    key     = "infra.tfstate"
-    region  = "ap-south-1"
-    profile = "default"
-    dynamodb_table = "vegeta-terraform-remote-state-table"
-  }
-}
